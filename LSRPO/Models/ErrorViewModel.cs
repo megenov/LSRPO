6c90f494d37db88d2cd575059f9cdcc2fce852bd
Мирослав Генов <genovme@gmail.com>
2022-02-26 22:47:32 +0200

    Add project files.

---
+++ LSRPO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LSRPO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
