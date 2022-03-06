e188da44f64ca9b18d4f7483182dc3c02c72c0ce
Мирослав Генов <genovme@gmail.com>
2022-03-06 02:27:58 +0200

    Added custom attribute

---
+++ LSRPO.Core/CustomAttributes/IsBeforeAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LSRPO.Core.CustomAttributes
+{
+    public class IsBeforeAttribute : ValidationAttribute
+    {
+        private readonly string propertyToCompare;
+
+        public IsBeforeAttribute(string propertyToCompare, string errorMessage = "")
+        {
+            this.propertyToCompare = propertyToCompare;
+            this.ErrorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                DateTime dateToCompare = (DateTime)validationContext
+                .ObjectType
+                .GetProperty(propertyToCompare)
+                .GetValue(validationContext.ObjectInstance);
+
+                if ((DateTime)value < dateToCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception)
+            {}
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
